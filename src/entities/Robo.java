7798c6644afc716a76d39421fea21f916fa1f7fc
LussanPH <166248501+LussanPH@users.noreply.github.com>
2024-07-23 15:43:15 -0300

    Update Robo.java

--- src/entities/Robo.java
+++ src/entities/Robo.java
@@ -2,13 +2,13 @@ package entities;
 import exception.MovimentoInvalidoException;
 
 public class Robo {
-	private String color;
-	private int [] coord = new int[2];
-	
+	protected String color;/**mudei para protected por causa da herança */
+	protected int [] coord = new int[2];
+	/**O this só é necessário quando tem variáveis com o mesmo nome, só no python que é preciso dele toda vez q chamar a variavel */
 	public Robo(String color) {
-		this.setColor(color);
-		this.getCoord()[0] = 0;
-		this.getCoord()[1] = 0;
+		setColor(color);
+		getCoord()[0] = 0;
+		getCoord()[1] = 0;
 	}
 
 	public String getColor() {
@@ -29,77 +29,83 @@ public class Robo {
 	
 	public void Mover(String mov) throws MovimentoInvalidoException{
 		if(mov.equals("up")) {
-			if(this.coord[1] >= 0) {
-				this.coord[1] += 1;
+			if(coord[1] >= 0) {
+				coord[1] += 1;
 			}
 			else {
 				throw new MovimentoInvalidoException("up");
 			}
 		}
 		else if(mov.equals("down")) {
-			if(this.coord[1] > 0) {
-				this.coord[1] -= 1;
+			if(coord[1] > 0) {
+				coord[1] -= 1;
 			}
 			else {
 				throw new MovimentoInvalidoException("down");
 			}
 		}
 		else if(mov.equals("right")) {
-			if(this.coord[0] >= 0) {
-				this.coord[0] += 1;
+			if(coord[0] >= 0) {
+				coord[0] += 1;
 			}
 			else {
 				throw new MovimentoInvalidoException("right");
 			}
 		}
 		else if(mov.equals("left")) {
-			if(this.coord[0] > 0) {
-				this.coord[0] -= 1;
+			if(coord[0] > 0) {
+				coord[0] -= 1;
 			}
 			else {
 				throw new MovimentoInvalidoException("left");
 			}
 		}
+        else{/**Para quando o usuario não utilizar nenhuma das opções */
+            throw new MovimentoInvalidoException();
+        }
 	}
 	
 	public void Mover(int mov) throws MovimentoInvalidoException{
 		if(mov == 1) {
-			if(this.coord[1] >= 0) {
-				this.coord[1] += 1;
+			if(coord[1] >= 0) {
+				coord[1] += 1;
 			}
 			else {
 				throw new MovimentoInvalidoException("up");
 			}
 		}
 		else if(mov == 2) {
-			if(this.coord[1] > 0) {
-				this.coord[1] -= 1;
+			if(coord[1] > 0) {
+				coord[1] -= 1;
 			}
 			else {
 				throw new MovimentoInvalidoException("down");
 			}
 		}
 		else if(mov == 3) {
-			if(this.coord[1] >= 0) {
-				this.coord[1] += 1;
+			if(coord[0] >= 0) {
+				coord[0] += 1;
 			}
 			else {
 				throw new MovimentoInvalidoException("right");
 			}
 		}
 		else if(mov == 4) {
-			if(this.coord[1] > 0) {
-				this.coord[1] -= 1;
+			if(coord[0] > 0) {
+				coord[0] -= 1;
 			}
 			else {
 				throw new MovimentoInvalidoException("left");
 			}
 		}
+        else{/**Para quando o usuario não utilizar nenhuma das opções */
+            throw new MovimentoInvalidoException();
+        }
 	}
 	
 	
 	public boolean isFoodFound(int[] food) {
-		if(this.coord[0] == food[0] && this.coord[1] == food[1]) {
+		if(coord[0] == food[0] && coord[1] == food[1]) {
 			return true;
 		}
 		else {
